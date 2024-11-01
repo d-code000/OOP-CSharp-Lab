3e627c0dd4fe61bcc3fab8541498ddcf32363e36
Disha <disha.wind@gmail.com>
2024-11-01 11:42:06 +0300

    Start point

---
+++ OOP-CSharp-Lab1/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace OOP_CSharp_Lab;
+
+public class Person
+{
+    private string Firstname;
+    private string Lastname;
+    private System.DateTime DateBirth;
+}
---
+++ OOP-CSharp-Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace OOP_CSharp_Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
