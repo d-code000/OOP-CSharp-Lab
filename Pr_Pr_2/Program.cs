1aafa6abba48bdf023d5c98ea5ac6f707c63d4fc
d_code000 <code.threezeros@gmail.com>
2024-11-15 11:32:42 +0300

    Start Lab 2

---
+++ Pr_Pr_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Pr_Pr_2;
+
+var test = new TestClass();
+test[10] = 555;
+test[3] = 333;
+test.AddElem(10);
+test.AddElems(10, 12, 23);
+test.AddElems(new List<int>{10, 25, 78});
+
+// var watch = new Stopwatch();
+// watch.Start();
+// test[40000000] = 999;
+// watch.Stop();
+// Console.WriteLine((double) watch.ElapsedMilliseconds/1000);
+
+Console.WriteLine(test);
---
+++ Pr_Pr_2/TestClass.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace Pr_Pr_2;
+
+public class TestClass
+{
+    private List<int> _ints = new List<int>();
+    private enum State
+    {
+        Positive, Negative, Natural, Unset
+    }
+
+    private State _state;
+
+    public TestClass()
+    {
+        _ints.Add(1);
+        _ints.Add(2);
+        _ints.Add(3);
+        _ints.Add(4);
+        _ints.Add(5);
+    }
+
+    public List<int> Ints
+    {
+        get
+        {
+            return new List<int>(_ints);
+        } 
+        private set => _ints = value;
+    }
+
+    public int this[int x]
+    {
+        get => _ints[x];
+        set
+        {
+            if (x >= _ints.Count )
+            {
+                for (int i = _ints.Count; i < x; i++) _ints.Add(0);
+                _ints.Add(value);
+            }
+            else
+            {
+                _ints[x] = value;
+            }
+        }
+    }
+
+    public string ListToString()
+    {
+        var sb = new StringBuilder();
+        foreach (var x in _ints)
+        {
+            sb.Append(x);
+            sb.Append(' ');
+        }
+        return sb.ToString();
+    }
+
+    public override string ToString()
+    {
+        return $"{_state}: {ListToString()}";
+    }
+
+    public void AddElem(int list)
+    {
+        _ints.Add(list);
+    }
+
+    public void AddElems(List<int> list)
+    {
+        _ints.AddRange(list);
+    }
+    
+    public void AddElems(params int[] list)
+    {
+        _ints.AddRange(list);
+    }
+}
